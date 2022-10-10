100823038b1ea125326f75c3bf8a29c18a142901
Saranyajothy <saranyajothikumar12@gmail.com>
2022-10-10 13:22:56 +0000

    remove bootstrap datepicker in requirement.text and delete the code in models.py

--- adminMod/models.py
+++ adminMod/models.py
@@ -32,27 +32,3 @@ class appointment(models.Model):
 class Item(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     done = models.BooleanField(null=False, blank=False)
-
-class BookingView(models.Model):
-    form_class = appointment()
-    template_name = "availability.html"
-
-    def form_valid(self, form):
-        data = form.cleaned_data
-        bookingList = Appointment.objects.filter()
-        
-        for booking in bookingList:
-            if booking.start > data["end_time"] or booking.end < data["start_time"]:
-                booking=Appointment.objects.create(
-                    name=data["name"], 
-                    start=data["start_time"],
-                    end=data["end_time"]
-                    )
-                booking.save()
-                print(booking.start)
-                print(data["start_time"])
-                return HttpResponse("can be booked")
-            else:
-                print(booking.start )
-                print(data["start_time"])
-                return HttpResponse("Cant be booked")
